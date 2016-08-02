1ca74a587b6b6ca79bbd4f70c626f6f3744a5211
wuhlcom <378433855@qq.com>
2016-08-02 02:08:31 -0400

    route users#index

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ DemoApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+   root 'users#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
